b1464273bcd9a1f81cd6da43d23961fe398af066
wwhite007 <167612851+wwhite007@users.noreply.github.com>
2026-05-25 11:13:39 +0800

    Update solution.py

--- hot100/0001-two-sum/solution.py
+++ hot100/0001-two-sum/solution.py
@@ -4,3 +4,11 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = {}
+        for i in range(len(nums)):
+            if nums[i] in hashtable:
+                return [hashtable[nums[i]], i]
+            hashtable[target - nums[i]] = i
