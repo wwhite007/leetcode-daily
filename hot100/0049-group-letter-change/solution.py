af9c362d3dfb1e661b5ffe18b9fbb87facec129f
Jingye Hu <167612851+wwhite007@users.noreply.github.com>
2026-07-25 15:12:36 +0800

    Update solution.py

--- hot100/0049-group-letter-change/solution.py
+++ hot100/0049-group-letter-change/solution.py
@@ -1,1 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        match_dict = {}
+        final_ans = []
+        for i in range(len(strs)):
+            ele = tuple(sorted(strs[i]))
+            if ele not in match_dict:
+                match_dict[ele] = {i} 
+            else:
+                match_dict[ele].add(i)
+        for ele in match_dict:
+            words = [] # 同一组字母异位词构成的列表。
+            for word_index in match_dict[ele]:
+                words.append(strs[word_index])
+            final_ans.append(words)
+        return final_ans
 
