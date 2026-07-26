8d0e2cb4b764af541989363fb99d136e3c3a4500
Jingye Hu <167612851+wwhite007@users.noreply.github.com>
2026-07-26 10:21:39 +0800

    Update solution2.py

--- hot100/0279-square-number/solution2.py
+++ hot100/0279-square-number/solution2.py
@@ -1,1 +1,22 @@
+from math import isqrt
 
+class Solution:
+    def numSquares(self, n: int) -> int:
+        def is_square(x: int) -> bool:
+            root = isqrt(x)
+            return root * root == x
+
+        if is_square(n):
+            return 1
+
+        while n % 4 == 0:
+            n //= 4
+
+        if n % 8 == 7:
+            return 4
+
+        for a in range(1, isqrt(n) + 1):
+            if is_square(n - a * a):
+                return 2
+
+        return 3
