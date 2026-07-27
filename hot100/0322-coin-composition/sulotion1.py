fccfe88baef39c4c77b0cc597ded5a017990be2f
Jingye Hu <167612851+wwhite007@users.noreply.github.com>
2026-07-27 09:58:08 +0800

    Update sulotion1.py

--- hot100/0322-coin-composition/sulotion1.py
+++ hot100/0322-coin-composition/sulotion1.py
@@ -1,1 +1,24 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = {}  # 代表当前的sum是i的时候，最后的结果是dp[i].
+        dp[0] = 0 
+        for i in range(0, amount+1):
+            if i == 0:
+                dp[i] = 0
+            elif i < min(coins):
+                dp[i] = amount+1
+            else:
+                for j in coins:
+                    if i < j:
+                        continue
+                    elif i not in dp:
+                        dp[i] = dp[i-j] + 1
+                    else:
+                        if j <= i:
+                            dp[i] = min(dp[i], dp[i-j]+1)
+        if dp[amount] > amount:
+            return -1
+        else:
+            return dp[amount] 
 
+        
