ee58b51bbda9e88562f922813e3ae8ec3fb85a3d
Jingye Hu <167612851+wwhite007@users.noreply.github.com>
2026-08-24 10:32:19 +0800

    Update solution2.py

--- hot100/0055-jumping-game/solution2.py
+++ hot100/0055-jumping-game/solution2.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        right = 0
+        for i in range(n-1):
+            if i <= right and nums[i] + i > right:
+                right = nums[i] + i
+            if right >= n-1:
+                return True
+        return right >= n-1
+        
